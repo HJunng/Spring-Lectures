ede8f90c36671023e5e7f54774cac27ad5ec2813
HJunng <guswjd1gh@gmail.com>
2023-10-11 18:39:50 +0900

    Member Test 구현

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    //눈으로 보면서 확인하는 print를 사용하지 않고, 검증 테스트를 통해 확인함.
+    @Test
+    void join(){
+
+        //given
+        Member member = new Member(1L,"memberA",Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+}
