3f821cdadb9f2c5d815875a1cee0a08a518f0ad2
HJunng <guswjd1gh@gmail.com>
2023-10-12 23:41:09 +0900

    MemberService, OrderService Test를 모두 의존관계 주입을 위해 AppConfig 선언 후 사용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach // 테스트를 실행하기 전에 무조건 실행 되는거
+    public void beforeConfig(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
 
     //눈으로 보면서 확인하는 print를 사용하지 않고, 검증 테스트를 통해 확인함.
     @Test
