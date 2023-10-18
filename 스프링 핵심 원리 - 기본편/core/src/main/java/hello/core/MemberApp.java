e23cc4ef8da07d6f7215180d153edfe0f98c14d5
HJunng <guswjd1gh@gmail.com>
2023-10-18 17:32:11 +0900

    스프링 컨테이너 사용 -> 의존관계 주입

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/MemberApp.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,19 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // AppConfig에서 다 결정함.
         //MemberService memberService = new MemberServiceImpl(); -> 기존에는 내가 멤버 서비스 인플을 직접 메인 메소드에서 생성 했었음.
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // AppConfig에서 다 결정함.
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
